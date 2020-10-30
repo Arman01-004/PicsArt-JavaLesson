7e9697e5f8f0ef52be526108c60ab6625e22fa9c
Arman01-004 <73685173+Arman01-004@users.noreply.github.com>
2020-10-30 15:43:17 +0400

    Add files via upload

---
+++ JavaLesson.java
@@ -0,0 +1,4 @@
+package picsart;
+
+public class JavaLesson {
+}
---
+++ Lesson1.java
@@ -0,0 +1,39 @@
+package picsart;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(" PicsArt ");
+        System.out.println("2020");
+
+        byte x = 78;
+        byte y = 20;
+        byte z = -56;
+        System.out.println(x+y+z);
+
+        int a = 4 ;
+
+                long b = 10248312084l;
+        System.out.println(b);
+
+        double bb = 3.333E02;
+        float xx = 547.123f;
+
+        boolean jj = true;
+        boolean gg = false;
+
+        char GL = '\u0021';
+        System.out.println(GL);
+        String name = "Arman";
+        System.out.println("Arman");
+
+
+    }
+}
+
+/*
+ctrl+space-hushum
+psvm-public static void main(String[] args)
+sout-SYstem.out.printLn
+CRTL+D-copy
+*/
